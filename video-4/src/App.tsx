2846d628c7f211e6ff1b922fe5ba2109851a2e0f
Subham Mondal <subhamoffi@gmail.com>
2025-05-18 21:36:03 +0530

    Completed video 4

--- video-4/src/App.tsx
+++ video-4/src/App.tsx
@@ -1,5 +1,5 @@
-import { useMotionValueEvent, useScroll, useTransform, motion } from "motion/react";
-import { useRef } from "react";
+import { useMotionValueEvent, useScroll, useTransform, motion, useMotionValue, useMotionTemplate, useSpring } from "motion/react";
+import { useEffect, useRef, useState } from "react";
 
 interface Features {
   icon: React.ReactNode;
@@ -14,7 +14,7 @@ function App() {
   //   console.log("Changed values: ", latest);
   //
   // })
-
+  const containerRef = useRef<HTMLDivElement>(null)
   const features: Features[] = [
     {
       icon: <div>ICON</div>,
@@ -44,9 +44,46 @@ function App() {
 
   ]
 
+  const backgrounds = ["#171717", "#0f172a", "#fff"]
+  const [background, setBackgrounds] = useState(backgrounds[0])
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end start"]
+  })
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    const finalValue = Math.floor(latest * backgrounds.length)
+    setBackgrounds(backgrounds[finalValue]);
+    console.log("Latest -->", latest);
+
+  })
+
+
+
   return (
     <>
-      <div className='min-h-screen bg-neutral-900 flex items-center justify-center'>
+      <motion.div
+        ref={containerRef}
+        initial={{
+          opacity: 0.5,
+          scale: 2,
+          filter: "blur(10px)"
+        }}
+        animate={{
+          scale: 1,
+          opacity: 1,
+          filter: "blur(0px)",
+          background
+        }}
+        transition={{
+          duration: 0.5,
+          ease: "easeInOut"
+        }}
+        style={{
+          background,
+        }}
+        className='min-h-screen bg-neutral-900 flex items-center justify-center'>
         <div className="flex py-40 flex-col gap-10 max-w-4xl mx-auto">
           {
             features.map((feature, idx) => {
@@ -55,7 +92,7 @@ function App() {
           }
         </div>
 
-      </div>
+      </motion.div>
     </>
   )
 }
@@ -67,17 +104,32 @@ const Card = ({ feature }: { feature: Features }) => {
     offset: ["start end", "end start"] //this means that when the start of the element reaches the viewport's end then it will start its function and end it when it reaches the viewport's start
   })
 
-  const translateContent = useTransform(scrollYProgress, [0, 1], [-200, 200])
-
+  const translateContent = useSpring(useTransform(scrollYProgress, [0, 1], [0, -400]),
+    {
+      mass: 10,
+      stiffness: 900,
+      damping: 100
+    }
+  )
+  const translateOpacity = useTransform(scrollYProgress, [0, 0.2, 0.6, 1], [0, 1, 0.5, 0])
+  const blur = useTransform(scrollYProgress, [0, 0.2, 0.5, 1], [10, 1, 0, 10])
+  const scale = useTransform(scrollYProgress, [0.5, 1], [1, 0.5])
   return <div ref={ref} className="grid grid-cols-2 gap-20 py-40">
-    <div className="flex flex-col gap-5">
+    <motion.div
+      style={{
+        filter: useMotionTemplate`blur(${blur}px)`,
+        scale,
+      }}
+      className="flex flex-col gap-5"
+    >
       <div className="text-sm text-white">{feature.icon}</div>
       <h1 className="text-4xl font-bold text-white">{feature.title}</h1>
       <p className="text-neutral-400">{feature.description}</p>
-    </div>
+    </motion.div>
 
     <motion.div style={{
-      y: translateContent
+      y: translateContent,
+      opacity: translateOpacity
     }} className="rounded-2xl w-full">{feature.content}</motion.div>
   </div>
 
